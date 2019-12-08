ddc3840cb63c00001fa3aa729ee5ecfa6fa6f963
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2019-12-08 17:30:55 -0500

    Improved multiresolution bitmap algorithm

--- MultiresolutionBitmap.hpp
+++ MultiresolutionBitmap.hpp
@@ -7,9 +7,7 @@ template <std::size_t R, std::size_t N>
 class MRBitmap {
 	private:
 		unsigned long long *bitmap[R];
-		double count[R];
-		int lower_bound;
-		int upper_bound;
+		long long count[R];
 		int h;
 	
 	public:
@@ -18,11 +16,12 @@ class MRBitmap {
 			for (int i = 0; i < R; i++) {
 				bitmap[i] = new unsigned long long[N / (d * 64) + 1];
 				d <<= 1;
+				count[i] = 0;
 			}
 		}
 
 		void insert (int idx) {
-			idx = idx % N;
+			idx = idx % (R * N);
 
 			int r = idx / N;
 
@@ -49,27 +48,24 @@ class MRBitmap {
 		}
 
 		int getCount() {
-			for (int i = 0 ; i < R; i++) {
-				double sizeR = N / (1 << i);
-				std::cout << std::endl << i << " " << count[i];
-				count[i] = (double) N * log(sizeR / (sizeR - count[i]));
-				std::cout << std::endl << i << " " << count[i] << std::endl;
-			}
-			
-			int base = R - 1;
-			while (base > 0 && count[base] <= 100000) {
-				base--;
+			int base = 1;
+			int setMax = (N / (1 << base)) * (1.0 - pow(2.7182, -3.1426));
+
+			while (base < R && count[base] <=  setMax) {
+				base++;
+				setMax = (N / (1 << base)) * (1.0 - pow(2.7182, -3.1426));
 			}
 
-			base++;
+			base--;
 
 			int m = 0;
-			for (int i = base; i < R; i++) {
-				m += N * log(N / (N - count[i]));
+			for (int i = 0; i <= base; i++) {
+				float sizeR = N / (1 << i);
+				m +=  sizeR * log(sizeR / (sizeR - count[i]));
 			}
 
-			int factor = (1 << (R - 1));
-			return factor * m;
+			int factor = 1 << (R - base - 1);
+			return m * R;
 		}
 };
 
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <vector>
 #include <unordered_set>
+#include <random>
 
 #include "DirectBitmap.hpp"
 #include "VirtualBitmap.hpp"
@@ -14,14 +15,15 @@ int main () {
 
 	DirectBitmap<NDBM> dbm;
 	VirtualBitmap<NVBM> vbm(0, 1000000, NDBM);
-	MRBitmap<4, 1000000> mrbm;
+	MRBitmap<5, 1000000> mrbm;
 
 	std::chrono::time_point<std::chrono::system_clock> start, end;
 	unsigned long long elapsed_time;
 
+	std::random_device rd;
 	std::vector<int> requests;
 	for (int i = 0; i < 10000000; i++) {
-		 requests.push_back(rand() % NDBM);
+		 requests.push_back(rd() % NDBM);
 	}
 
 	std::unordered_set<int> st;
@@ -32,7 +34,7 @@ int main () {
 	end = std::chrono::system_clock::now();
 
 	elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-	std::cout << "Control: " << elapsed_time << std::endl;
+	std::cout << "Control: " << elapsed_time << " " << st.size() << std::endl;
 
 	start = std::chrono::system_clock::now();
 	for (int r : requests) {
@@ -41,7 +43,7 @@ int main () {
 	end = std::chrono::system_clock::now();
 
 	elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-	std::cout << "Direct Bitmap: " << elapsed_time << std::endl;
+	std::cout << "Direct Bitmap: " << elapsed_time << " " << dbm.getCount() << std::endl;
 
 	start = std::chrono::system_clock::now();
 	for (int r : requests) {
@@ -50,7 +52,7 @@ int main () {
 	end = std::chrono::system_clock::now();
 
 	elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-	std::cout << "Virtual Bitmap: " << elapsed_time << std::endl;
+	std::cout << "Virtual Bitmap: " << elapsed_time << " " << vbm.getCount() << std::endl;
 
 	start = std::chrono::system_clock::now();
 	for (int r : requests) {
@@ -59,7 +61,7 @@ int main () {
 	end = std::chrono::system_clock::now();
 
 	elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-	std::cout << "Multi-resolution Bitmap: " << elapsed_time << std::endl;
+	std::cout << "Multi-resolution Bitmap: " << elapsed_time << " " << mrbm.getCount() << std::endl;
 	
 	return 0;
 }
